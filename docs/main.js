4248a45d336318b59fb1fb538222a4494989094f
ilham B <durianbohong@gmail.com>
2022-12-16 19:47:20 +0700

    modified:   docs/index.html 	new file:   docs/main.js 	renamed:    web/node_modules/.package-lock.json -> docs/node_modules/.package-lock.json 	renamed:    web/node_modules/newdom/LICENSE -> docs/node_modules/newdom/LICENSE 	renamed:    web/node_modules/newdom/README.md -> docs/node_modules/newdom/README.md 	renamed:    web/node_modules/newdom/index.js -> docs/node_modules/newdom/index.js 	renamed:    web/node_modules/newdom/nativeJS.js -> docs/node_modules/newdom/nativeJS.js 	renamed:    web/node_modules/newdom/package.json -> docs/node_modules/newdom/package.json 	renamed:    web/package-lock.json -> docs/package-lock.json 	renamed:    web/package.json -> docs/package.json 	deleted:    web/index.html

---
+++ docs/main.js
@@ -0,0 +1,24 @@
+import { DOM } from "./node_modules/newdom/nativeJS.js";
+
+window.addEventListener('load', () => {
+
+    let content = DOM("div", {
+        attr : {class: "container bg-danger"},
+        inner: [
+
+            DOM("h1", {
+                attr: {class: "text-center text-white"},
+                inner: "BINARY CALCULATOR"
+            })
+        ]
+    });
+
+    loadStyle("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
+
+    document.body.prepend(content);
+})
+
+function loadStyle(link) {
+
+    document.head.appendChild(DOM("link", {attr : {rel: "stylesheet", href: link}}));
+}
